2291606ee513726b85ada0c8911b9a5c40750ef0
phil cowan <philipjcowan1@gmail.com>
2019-12-13 09:44:23 -0700

    adds script for computer and userGuess and if else to add to wins or losses variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,20 @@
-alert("hello")
+// we'll need a variable for the computer's letter, the users guess, the number of wins, number of losses, and the amount of guesses left, and the list of previous guesses
+
+// this function will run when the user presses a letter key
+var computerChoices = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    
+    // random computer choice
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+if (userGuess === computerGuess) {
+    wins++
+}
+else {
+    losses++
+}
+var wins = 0
+var losses = 0
+var [soFar]
