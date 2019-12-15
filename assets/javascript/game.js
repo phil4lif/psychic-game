4e1755068dee595f392b70c72566fba7e1cb15c5
phil cowan <philipjcowan1@gmail.com>
2019-12-14 23:47:01 -0700

    fixed the if else statement typo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
 // we'll need a variable for the computer's letter, the users guess, the number of wins, number of losses, and the amount of guesses left, and the list of previous guesses
-var wins = 0
-var losses = 0
-var guessLeft = 9
-// this function will run when the user presses a letter key
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+
+
+// creates an array of the alphabet for the computer to choose from
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 document.onkeyup = function(event) {
     var userGuess = event.key;
     
@@ -11,18 +15,19 @@ document.onkeyup = function(event) {
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 
-if (userGuess === computerGuess) {
+if ((userGuess === computerGuess)) {
     wins++;
-    }
+    guessesLeft--;
+}
 
     else {
     losses++;
-    guessLeft--;
-    }
+    guessesLeft--;
+}
 };
 
 
-document.getElementById("losses").innerHTML = losses;
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("guessesleft").innerHTML = guessLeft;
+document.getElementById("losses").textContent = losses;
+document.getElementById("wins").textContent = wins;
+document.getElementById("guessesleft").textContent = guessesLeft;
 
