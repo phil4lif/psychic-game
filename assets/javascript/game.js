b08588ec309f3adddb8fd81ec9a887ab388edc21
phil cowan <philipjcowan1@gmail.com>
2019-12-15 00:00:32 -0700

    trying a new approach, makes vars to reference html places

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,27 @@
-// we'll need a variable for the computer's letter, the users guess, the number of wins, number of losses, and the amount of guesses left, and the list of previous guesses
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
 
 
 
 // creates an array of the alphabet for the computer to choose from
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+// we'll need a variable for the computer's letter, the users guess, the number of wins, number of losses, and the amount of guesses left, and the list of previous guesses
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessedSoFar = "";
+
+//create varialbes that hold reference to html ids where we want to display info
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var guessesLeftText = document.getElementById("guessesleft");
+var soFarText = document.getElementById("sofar");
+
+
+
+
+
+
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
@@ -27,7 +42,4 @@ if ((userGuess === computerGuess)) {
 };
 
 
-document.getElementById("losses").textContent = losses;
-document.getElementById("wins").textContent = wins;
-document.getElementById("guessesleft").textContent = guessesLeft;
 
