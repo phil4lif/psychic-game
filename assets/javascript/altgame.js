c00933eb38b7e6815e762ad46337436ba261c1f5
phil cowan <philipjcowan1@gmail.com>
2019-12-19 13:17:15 -0700

    makes an altjs file to try to redo the javascript in a more organized way

---
+++ assets/javascript/altgame.js
@@ -0,0 +1,49 @@
+//Declare variables for wins losses letters guessed the computer letter
+
+var wins = 0
+var losses = 0
+var lettersGuessed = [""]
+var compLetter = ""
+var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+
+//make a function that makes the computer choose a random letter
+var letterGenerator=function(){
+    compLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(compLetter);
+}
+//makes a newgame function
+var newGame = function(){
+    letterGenerator();
+    lettersGuessed = [""];
+}
+
+//make a function for a victory
+var winFunction = function(){
+    wins++;
+    newGame();
+}
+
+//make a function for a loss
+var lossFunction = function(){
+    losses--;
+    newGame();
+}
+
+
+//GAME PLAY SECTION
+//the user enters a guess
+
+
+
+//checks to see if the guess is correct
+
+
+//if it is correct runs the victory function
+
+
+
+//adds to lettersGuessed if incorrect
+
+//if the user has used all of their guesses then run the victory function
+
