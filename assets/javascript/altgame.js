bd1800c6a4416dd24b374498a439f5b20b984718
phil cowan <philipjcowan1@gmail.com>
2019-12-19 13:38:48 -0700

    makes more javascript, got to as far as checking for the correct letter and invoking the win correction if so

--- assets/javascript/altgame.js
+++ assets/javascript/altgame.js
@@ -1,11 +1,12 @@
 //Declare variables for wins losses letters guessed the computer letter
 
-var wins = 0
-var losses = 0
-var lettersGuessed = [""]
-var compLetter = ""
+var wins = 0;
+var losses = 0;
+var lettersGuessed = [""];
+var compLetter = "";
 var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
+var userGuess = "";
+var guessesLeft = 9;
 
 //make a function that makes the computer choose a random letter
 var letterGenerator=function(){
@@ -16,6 +17,7 @@ var letterGenerator=function(){
 var newGame = function(){
     letterGenerator();
     lettersGuessed = [""];
+    guessesLeft = 9;
 }
 
 //make a function for a victory
@@ -33,6 +35,19 @@ var lossFunction = function(){
 
 //GAME PLAY SECTION
 //the user enters a guess
+if (guessesLeft > 0){
+document.onkeyup=function(event){
+    userGuess = event.key;
+    if (userGuess===compLetter) {
+        winFunction()
+    }
+    else{
+        guessesLeft--
+    } 
+        
+}
+}
+
 
 
 
