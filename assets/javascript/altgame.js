cb95609b3780fab138913701c81ad0ef2fae8145
phil cowan <philipjcowan1@gmail.com>
2019-12-19 17:16:33 -0700

    adds variables that will refer to the id s on the html page

--- assets/javascript/altgame.js
+++ assets/javascript/altgame.js
@@ -2,12 +2,16 @@
 
 var wins = 0;
 var losses = 0;
-var lettersGuessed = [""];
+var guessedSoFar = [""];
 var compLetter = "";
 var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var userGuess = "";
 var guessesLeft = 9;
-
+//variables that will refer to the html page
+var guessedSoFarText = document.getElementById("#sofar");
+var winsText= document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var guessesLeftText= document.getElementById("guessesleft");
 //make a function that makes the computer choose a random letter
 var letterGenerator=function(){
     compLetter = letters[Math.floor(Math.random() * letters.length)];
@@ -16,7 +20,7 @@ var letterGenerator=function(){
 //makes a newgame function
 var newGame = function(){
     letterGenerator();
-    lettersGuessed = [""];
+    guessedSoFar = [""];
     guessesLeft = 9;
 }
 
@@ -32,17 +36,19 @@ var lossFunction = function(){
     newGame();
 }
 
-
 //GAME PLAY SECTION
 //the user enters a guess
-if (guessesLeft > 0){
 document.onkeyup=function(event){
     userGuess = event.key;
+if (guessesLeft > 0){
+
     if (userGuess===compLetter) {
         winFunction()
     }
     else{
-        guessesLeft--
+        guessesLeft--;
+        guessedSoFar.push(userGuess);
+        
     } 
         
 }
