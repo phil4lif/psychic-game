f640b243f93081d1fe555f72e229e7d00cc54044
phil cowan <philipjcowan1@gmail.com>
2019-12-19 17:56:43 -0700

    has everything working but does not display guesses so far

--- assets/javascript/altgame.js
+++ assets/javascript/altgame.js
@@ -4,67 +4,88 @@ var wins = 0;
 var losses = 0;
 var guessedSoFar = [""];
 var compLetter = "";
-var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var userGuess = "";
 var guessesLeft = 9;
 //variables that will refer to the html page
 var guessedSoFarText = document.getElementById("#sofar");
-var winsText= document.getElementById("wins");
+var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
-var guessesLeftText= document.getElementById("guessesleft");
+var guessesLeftText = document.getElementById("guessesleft");
 //make a function that makes the computer choose a random letter
-var letterGenerator=function(){
+var letterGenerator = function () {
     compLetter = letters[Math.floor(Math.random() * letters.length)];
     console.log(compLetter);
 }
 //makes a newgame function
-var newGame = function(){
+var newGame = function () {
     letterGenerator();
-    guessedSoFar = [""];
+    guessedSoFarText = [""];
     guessesLeft = 9;
+
 }
 
 //make a function for a victory
-var winFunction = function(){
+var winFunction = function () {
     wins++;
     newGame();
 }
 
 //make a function for a loss
-var lossFunction = function(){
-    losses--;
+var lossFunction = function () {
+    losses++;
     newGame();
 }
 
 //GAME PLAY SECTION
 //the user enters a guess
-document.onkeyup=function(event){
-    userGuess = event.key;
-if (guessesLeft > 0){
+document.onkeyup = function (e) {
+    if (e.keyCode == 32) {
+        newGame();
 
-    if (userGuess===compLetter) {
-        winFunction()
+        document.onkeyup = function (event) {
+            userGuess = event.key;
+            console.log(userGuess);
+
+
+            if (guessesLeft > 0) {
+
+                if (userGuess === compLetter) {
+                    winFunction()
+                }
+                else {
+                    guessesLeft--;
+                    guessedSoFar.push(userGuess);
+                    console.log(guessedSoFar);
+                    // guessedSoFarText.innerHTML="" + guessedSoFar.join(" ");
+                    // guessedSoFarText.textContent = guessedSoFar[" "];
+                    guessedSoFarText.textContent = guessedSoFar;
+
+                }
+                if (guessesLeft === 0) {
+                    lossFunction()
+                }
+
+                winsText.textContent = wins;
+                lossesText.textContent = losses;
+                guessesLeftText.textContent = guessesLeft;
+
+            }
+
+        }
     }
-    else{
-        guessesLeft--;
-        guessedSoFar.push(userGuess);
-        
-    } 
-        
+
+
+
+    //checks to see if the guess is correct
+
+
+    //if it is correct runs the victory function
+
+
+
+    //adds to lettersGuessed if incorrect
+
+    //if the user has used all of their guesses then run the victory function
+
 }
-}
-
-
-
-
-//checks to see if the guess is correct
-
-
-//if it is correct runs the victory function
-
-
-
-//adds to lettersGuessed if incorrect
-
-//if the user has used all of their guesses then run the victory function
-
